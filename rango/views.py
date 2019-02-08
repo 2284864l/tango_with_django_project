f36cd6efde09b37869134edf3a125e8a3a234a1f
SHENGXIAN LIU <2284864l@student.gla.ac.uk>
2019-02-08 10:00:51 +0000

    chapter 10 complete

--- rango/views.py
+++ rango/views.py
@@ -6,38 +6,48 @@ from django.contrib.auth import authenticate, login,logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 def homepage(requeest):
     return HttpResponse("Homepage"
                         "<br>"
                         "Rango says hey there partner!"
-                    	"<br/>"
+                        "<br/>"
                         "<a href='/rango/'>Index</href>"
                         "<br>"
-						"<a href='/rango/about/'>About</href>")
+                        "<a href='/rango/about/'>About</href>")
 
 def index(request):
-	category_list = Category.objects.order_by('-likes')[:5]
-	page_list = Page.objects.order_by('-views')[:5]
-	context_dict = {'categories':category_list,'pages':page_list}
-	return render(request, 'rango/index.html', context_dict)
+        request.session.set_test_cookie()
+        category_list = Category.objects.order_by('-likes')[:5]
+        page_list = Page.objects.order_by('-views')[:5]
+        context_dict = {'categories': category_list, 'pages': page_list}
+        visitor_cookie_handler(request)
+        context_dict['visits'] = request.session['visits']
+        response = render(request, 'rango/index.html', context=context_dict)
+        return response
 
 
 def about(request):
-	context_dict = {'author': "SHENGXIAN LIU: 2284864L"}
-	return render(request, 'rango/about.html', context=context_dict)
+        visitor_cookie_handler(request)
+        if request.session.test_cookie_worked():
+            print("TEST COOKIE WORKED")
+            request.session.delete_test_cookie()
+        context_dict = {'author': "SHENGXIAN LIU: 2284864L"}
+        context_dict['visits'] = request.session['visits']
+        return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
-	context_dict = {}
-	try:
-		category = Category.objects.get(slug=category_name_slug)
-		pages = Page.objects.filter(category=category)
-		context_dict['pages'] = pages
-		context_dict['category'] = category
-	except Category.DoesNotExist:
-		context_dict['category'] = None
-		context_dict['pages'] = None
-	return render(request, 'rango/category.html', context_dict)
+        context_dict = {}
+        try:
+            category = Category.objects.get(slug=category_name_slug)
+            pages = Page.objects.filter(category=category)
+            context_dict['pages'] = pages
+            context_dict['category'] = category
+        except Category.DoesNotExist:
+            context_dict['category'] = None
+            context_dict['pages'] = None
+        return render(request, 'rango/category.html', context_dict)
 
 def add_category(request):
     form = CategoryForm()
@@ -128,3 +138,22 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,  'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0:
+        visits += 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
+
+def get_server_side_cookie(request, cookie, default_val=None):
+        val = request.session.get(cookie)
+        if not val:
+                val = default_val
+        return val
