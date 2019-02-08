39d6bc8e2fc1f078e84b0e614b75773ccdbbb7b8
SHENGXIAN LIU <2284864l@student.gla.ac.uk>
2019-02-08 00:07:37 +0000

    chapter 7 complete

--- rango/models.py
+++ rango/models.py
@@ -1,29 +1,30 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 
-
 class Category(models.Model):
-        name = models.CharField(max_length=128, unique=True)
-        views = models.IntegerField(default=0)
-        likes = models.IntegerField(default=0)
-        slug = models.SlugField(unique=True)
+	max_name_length = 128
+	name = models.CharField(max_length=max_name_length, unique=True)
+	views = models.IntegerField(default=0)
+	likes = models.IntegerField(default=0)
+	slug = models.SlugField()
 
-        def save(self, *args, **kwargs):
-                self.slug = slugify(self.name)
-                super(Category, self).save(*args, **kwargs)
-        class Meta:
-                verbose_name_plural = 'categories'
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Category, self).save(*args, **kwargs)
 
-        def __str__(self):
-                return self.name
+	class Meta:
+		verbose_name_plural = 'Categories'
+
+	def __str__(self): # For Python 2, use __unicode__ too
+		return self.name
 
 class Page(models.Model):
+	max_title_length = 128
 	category = models.ForeignKey(Category)
-	title = models.CharField(max_length=128)
+	title = models.CharField(max_length=max_title_length)
 	url = models.URLField()
 	views = models.IntegerField(default=0)
 
 	def __str__(self): # For Python 2, use __unicode__ too
 		return self.title
-
 
